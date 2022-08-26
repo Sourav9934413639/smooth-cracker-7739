219283cf902d001a973ae794bede80bf216b7992
salonidhatrak <saloni123@gmail.com>
2022-08-27 01:07:09 +0530

    day 4 task completed

--- sports.js
+++ sports.js
@@ -1,43 +1,33 @@
-// let url = 'https://newsdata.io/api/1/news?apikey=pub_10560ccfda5180ad3a9e3c95dbd133c40005d&q=sports&country=in&language=en&category=sports'
+let url = 'https://newsapi.org/v2/top-headlines?country=in&category=Sports&apiKey=3fbeddfb8b474c858aded713acc19c9c';
 
-// pub_10560639d33c71b60f5e8995fe804af001366
+let getData = async () =>{
 
-let url = 'https://newsdata.io/api/1/news?apikey=pub_10560639d33c71b60f5e8995fe804af001366&category=sports'
-
-// let url = 'https://newsdata.io/api/1/news?apikey=pub_10560639d33c71b60f5e8995fe804af001366&q=sports&country=cn,in,jp&category=sports '
-
-// let url = 'https://newsdata.io/api/1/news?apikey=pub_10560639d33c71b60f5e8995fe804af001366'
-
-async function getData () {
-  
-    try{
-        let res = await fetch (url);
-        let data = await res.json();
-        console.log(data)
-        append(data.results)
-    }catch(err){
-        console.log(err)
-    }
+    let res = await fetch(url);
+    let data = await res.json();
+    console.log(data)
+    append(data.articles)
 }
-getData()
+
+getData();
 
 
-
-let append = (data) =>{
+let append = (data)=> {
 
     data.forEach((el)=>{
 
         let div = document.createElement('div');
 
-        let img = document.createElement('img');
-        img.src = el.image_url;
+        let img = document.createElement('img')
+        img.src = el.urlToImage;
 
-        let title = document.createElement('h3');
-        title.innerText = el.title
+        let title  = document.createElement('h3')
+        title.innerText = el.title;
 
-        // console.log(img,title)
-        div.append(img,title);
+        let desc = document.createElement('p');
+        desc.innerText = el.description;
+
+
+        div.append(img,title,desc);
         document.getElementById('cont').append(div)
     })
-
 }
